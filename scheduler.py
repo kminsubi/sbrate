f7650c3fceabb662662807a8cc9e5931670e79e0
kminsubi <kminsubi@gmail.com>
2026-08-19 23:43:59 +0900

    Enable rate simulation V2

--- scheduler.py
+++ scheduler.py
@@ -13,6 +13,7 @@ from datetime import datetime
 from apscheduler.schedulers.background import BackgroundScheduler
 from data_quality_runtime import install_data_quality_endpoint
 from rate_simulator import install_rate_simulator
+from rate_simulator_v2 import install_rate_simulator_v2
 from visitor_platform import enable_mobile_platform_detection
 from visitor_stats import install_visitor_stats_hooks
 
@@ -131,3 +132,4 @@ enable_mobile_platform_detection()
 install_visitor_stats_hooks()
 install_data_quality_endpoint()
 install_rate_simulator()
+install_rate_simulator_v2()
